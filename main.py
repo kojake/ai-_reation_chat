329731f878006428fdcb7c63826d947fa8e72d75
kaito <takahashikaito@takahashikaito23.local>
2023-05-11 22:01:53 +0900

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ with open("Conversation_list.json", "r", encoding="utf-8") as file:
 print("アルゴリズム会話")
 print("_______________")
 print("会話を始めるなら：1")
+print("アルゴリズムを削除する：2")
 
 waiting_for_choices = input("番号を入力して下さい ==>")
 
@@ -41,3 +42,21 @@ if waiting_for_choices == "1":
             print("学習させましょう")
             Content_you_want_to_reply_to = input("なんて返信させたいですか？ ==>")
             Conversation_list[Waiting_for_conversation_reply] = Content_you_want_to_reply_to
+elif waiting_for_choices == "2":
+    print("________________!dangerzone!________________")
+    print("アルゴリズムを削除してしまいますと元に戻せません")
+    print("終わる場合は「exit」と入力して下さい")
+    print("____________________________________________")
+    print("あなたが学習させたアルゴリズムリストです")
+    for item in Conversation_list.items():
+        print("返信" + item[0] + "回答" + item[1])
+        Waiting_for_delete_selection = input("このアルゴリズムは削除しますか y/n ==>")
+
+        if Waiting_for_delete_selection == "y":
+            print("_______________________________")
+            print("選択されたアルゴリズム")
+            print("返信" + item[0] + "回答" + item[1])
+            print("-------------------------------")
+            Final_confirmation_of_deletion = input("本当に削除しますか この操作は取り消せません？ y/n")
+        else:
+            continue
