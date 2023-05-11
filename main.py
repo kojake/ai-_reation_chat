17bbec43259048d09945c810487ec56b020cc519
kaito <takahashikaito@takahashikaito23.local>
2023-05-11 21:21:50 +0900

    update

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-Conversation_list = {"a":"b", "b":"c"}
+import json
+#jsonからデータを読み込む
+with open("Conversation_list.json", "r", encoding="utf-8") as file:
+    Conversation_list = json.load(file)
 
 print("アルゴリズム会話")
 print("_______________")
 print("会話を始めるなら：1")
+
 waiting_for_choices = input("番号を入力して下さい ==>")
 
 if waiting_for_choices == "1":
@@ -29,5 +33,11 @@ if waiting_for_choices == "1":
                 print("学習させましょう")
                 Content_you_want_to_reply_to = input("なんて返信させたいですか？ ==>")
                 Conversation_list[Waiting_for_conversation_reply] = Content_you_want_to_reply_to
+                #jsonファイルに追加し保存する
+                with open("Conversation_list.json", "w", encoding="utf-8") as file:
+                    json.dump(Conversation_list, file)
         else:
-            Conversation_list
+            print("アルゴリズムはこの言葉を理解していません")
+            print("学習させましょう")
+            Content_you_want_to_reply_to = input("なんて返信させたいですか？ ==>")
+            Conversation_list[Waiting_for_conversation_reply] = Content_you_want_to_reply_to
